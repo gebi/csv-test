9655167114c81a50427391a63aef769564a4dfa5
Michael Gebetsroither <michael@mgeb.org>
2013-11-06 15:27:11 +0100

    python: add option to skip leading spaces

--- csv-test.py
+++ csv-test.py
@@ -2,8 +2,14 @@
 
 import csv
 import sys
+import optparse
 
-spamreader = csv.reader(sys.stdin)
+parser = optparse.OptionParser()
+parser.add_option("--trim_leading_space", dest="trim_leading_space", action='store_true',
+        default=False, help="Trim leading space on begin of csv records")
+(opts, args) = parser.parse_args()
+
+spamreader = csv.reader(sys.stdin, skipinitialspace=opts.trim_leading_space)
 lineno = 1
 for row in spamreader:
     elemno = 0
